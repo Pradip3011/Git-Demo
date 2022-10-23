2a53f1ffa08e141b641d55821bccd0b689646a6d
Pradip3011 <vickey.jha35@gmail.com>
2022-10-23 13:28:12 +0530

    added window handlers functionality

--- src/OpenBrowser.java
+++ src/OpenBrowser.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class OpenBrowser {
 
-		WebDriver driver;
+		WebDriver wb;
 		
 		@Test
 		@Parameters("browser")
@@ -15,18 +15,24 @@ public class OpenBrowser {
 			if(bname.equalsIgnoreCase("chrome"))
 			{
 				System.setProperty("webdriver.chrome.driver",".\\Driver\\chromedriver.exe");
-				driver = new ChromeDriver();
+				wb = new ChromeDriver();
 			}
 			
 			else if(bname.equalsIgnoreCase("firefox"))
 			{
 				System.setProperty("webdriver.gecko.driver",".\\Driver\\geckodriver.exe");
-				driver = new FirefoxDriver();
+				wb = new FirefoxDriver();
 			}
 			
-			driver.manage().window().maximize();
-			driver.get("https:\\www.google.com");
-			System.out.println(driver.getTitle());
+			wb.manage().window().maximize();
+			GoToPage obj = new GoToPage(wb);
+			obj.openPage();
+		}
+		@Test
+		public void flow()
+		{
+			
+			
 		}
 
 }
